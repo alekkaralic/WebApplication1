9961ef4dcf3b2d4fa6407c9e6170579e8beec0d1
mkaralic <miki.karalic@gmail.com>
2018-02-04 15:34:36 +0100

    asd

---
+++ WebApplication1/scripts/Index.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="jquery-3.3.1.intellisense.js" />
+$(document).ready(function () {
+    $("#izracunajBtn").click(function () {
+        document.getElementById("rezultat").innerHTML = "rezyltat";
+    });
+});
