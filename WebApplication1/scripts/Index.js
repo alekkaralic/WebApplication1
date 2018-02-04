5eaafc096da65004678882ca32464c2696dc8afa
mkaralic <miki.karalic@gmail.com>
2018-02-04 16:12:24 +0100

    aaaa

--- WebApplication1/scripts/Index.js
+++ WebApplication1/scripts/Index.js
@@ -1,6 +1,7 @@
 ﻿/// <reference path="jquery-3.3.1.intellisense.js" />
 $(document).ready(function () {
     $("#izracunajBtn").click(function () {
+        // ovde ide kod
         document.getElementById("rezultat").innerHTML = "rezyltat";
     });
 });
